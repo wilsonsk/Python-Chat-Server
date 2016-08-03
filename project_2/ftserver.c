4c99097d21ab217fa120a2fee3187fb207765ce9
kira antoku <Yasuko@kiras-MacBook-Air.local>
2016-08-02 21:25:21 -0700

    ftserver requirements

--- project_2/ftserver.c
+++ project_2/ftserver.c
@@ -12,7 +12,28 @@
 *       4. ftserver and ftclient establist a TCP 'control' connection on <SERVER_PORT>. (call this connection C)
 *	5. ftserver waits on connection P for ftclient to send command
 *	6. ftclient sends command (-l (list) or -g <FILENAME> (get)) on connection C
-*	7. 
+*	7. ftserver receives command on connection C
+		* if ftclient sent an invalid command:
+			* ftserver sends an error message to ftclient on connection C, and
+			ftclient displays the message on-screen
+		* otherwise:
+			* ftserver initiates a TCP 'data' connection with ftclient on
+			<DATA_PORT> (call this connection D)
+			* if ftclient has sent the -l command, ftserver sends its directory to 
+			ftclient on connection D, and ftclient displays the directory on-screen
+			* if ftclient has sent -g <FILENAME> command, ftserver validates FILENAME
+			and either:
+				* sends the contents of FILENAME on connection D, ftclient saves
+				the file in the current default directory (handling "duplicate file
+				name" error if necessary) and displays a "transfer complete" message
+				on-screen
+			* or:
+				* sends an appropriate error message ("File not found", etc.) to ftclient
+				on connection C and ftclient displays the message on-screen
+			* ftserver closes connection D (don't leave open sockets!)
+* 	8. ftclient closes connection C (don't leave open sockets!)
+*	9. ftserver repeats from 2 (above) until terminated by a supervisor (SIGINT)  
+
         * ftclient is implemented in python
         * ftserver is implemented in c
 * references:
