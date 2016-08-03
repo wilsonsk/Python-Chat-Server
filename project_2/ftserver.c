551621aa57070da1bcf396bcc2857903b75afc87
root <root@localhost.localdomain>
2016-08-03 14:51:48 -0700

    socket creation, socket initialization in FTP function

--- project_2/ftserver.c
+++ project_2/ftserver.c
@@ -55,9 +55,13 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define c_portno_min	1024
+#define c_portno_max	65535
+
 
 void checkArgs(int agrc, char** argv, int s_portno);
 int checkPortArgInt(char* portArg, int* s_portno);
+void ftp(int s_portno);
 
 int main(int argc, char** argv){
 	int s_portno;
@@ -73,9 +77,14 @@ int main(int argc, char** argv){
 		* char** argv -- a pointer to pointers of type char -- an array of char pointers (c strings) -- an array of command line arguments
 		* int s_portno -- holds the <SERVER_PORT> upon conversion to integer
 	* outputs:
-		* 
+		* on success -- void
+		* on failure -- exit(1)
+	* calls: 
+		* int checkPortArg(char* portArg, int* s_portno)
 	* purpose:
-		* 
+		* checks the number of command line arguments
+		* checks that argument 1 <SERVER_PORT> is an integer 
+		* checks that argument 1 <SERVER_PORT> is within range
 */
 void checkArgs(int argc, char** argv, int s_portno){
 	if(argc != 2){
@@ -90,7 +99,7 @@ void checkArgs(int argc, char** argv, int s_portno){
 	}
 
 	// check s_portno range [1024, 65535]
-	if(s_portno < 1024 || s_portno > 65535){
+	if(s_portno < c_portno_min || s_portno > c_portno_max){
 		fprintf(stderr, "error: ftserver port number must be between 1024-65535\n");
 		exit(1);
 	}
@@ -100,7 +109,7 @@ void checkArgs(int argc, char** argv, int s_portno){
 /* int checkPortArg function: 
 	* inputs:
 		* char* portArg -- holds the <SERVER_PORT> argument from command line
-		* int* s_portno -- pointer to variable that to hold <SERVER_PORT> upon conversion to integer
+		* int* s_portno -- pointer to variable that to hold <SERVER_PORT> arugment from command line upon conversion to integer
 	* outputs:
 		* returns 1 if portArg is an integer; returns 0 if portArg is not an integer
 	* purpose:
@@ -117,6 +126,60 @@ int checkPortArgInt(char* portArg, int* s_portno){
 	return matches == 1;
 }
 
+/* void ftp(int s_portno)
+	* inputs:
+		* int s_portno -- holds the <SERVER_PORT> argument from command line upon conversion to integer
+	* outputs:
+		* on success -- 
+		* on failure -- perrer(<error_message>) exit(1)
+	* calls:
+		* 
+	* purpose:
+		* create server socket; bind server socket with server address and server port number
+		* listen for incoming connections on socket
+		* provide FTP for client connections
+			* create/maintain Control connection
+			* create Data connection
+		* end FTP on interrupt signal
+*/
+void ftp(int s_portno){
+	//file descriptor for server socket
+	int s_sockfd;
+	//server socket address
+	//The variable serv_addr is a structure of type struct sockaddr_in. This structure has four fields
+	struct sockaddr_in serv_addr;
+	
+		
+	//creating the socket
+	//arg1 = domain (AF_INET for IP); arg2 = transport protocol type (UDP = SOCK_DGRAM, TCP = SOCK_STREAM); arg3 = protocol (IP = 0)
+	//check if socket was created
+	if((s_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+		perror("socket creation");
+		exit(1);
+	}
+
+	// --initialize serv_addr struct: 4 fields -- //
+
+	//zero out serv_addr
+	bzero((char*) &serv_addr, sizeof(serv_addr));	
+
+	//The first field is short sin_family, which contains a code 
+	//for the address family. It should always be set to the symbolic constant AF_INET: IPv4
+	serv_addr.sin_family = AF_INET;
+	
+	//set port number of socket to portno arg
+	//The second field is an unsigned short: sin_port
+	//The htons(hostshort) function converts the unsigned short integer hostshort from host byte order to network byte order (i.e., Big Endian conversion)
+	serv_addr.sin_port = htons(s_portno);
+	
+	//The third field is a structure of type struct in_addr which contains only a single field: unsigned long s_addr
+	//field: unsigned long s_addr contains the IP address of the host; for a serverthis will always be the IP address of the machine on which the server is running, and there
+	//is a symbolic constant INADDR_ANY which gets the address of localhost
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+	//bind server socket with host address and server portno
+	int status = bind(s_sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
 
 
+}
 
